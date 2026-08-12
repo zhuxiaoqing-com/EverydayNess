4d2bac0aa9278c43eaebd3fab4f4fc419a720cb2
zhuxiaoqing <1079622162@qq.com>
2026-08-12 20:23:49 +0800

    增加NodeType

---
+++ core/gencode/apt-annotation/src/main/java/org/evd/game/annotation/NodeType.java
@@ -0,0 +1,13 @@
+package org.evd.game.annotation;
+
+/**
+ * Node 的服务器角色。
+ */
+public enum NodeType {
+    /** 游戏服 Node。 */
+    GAME,
+    /** 小跨服 Node。 */
+    ZONE,
+    /** 大跨服 Node。 */
+    GLOBAL
+}
